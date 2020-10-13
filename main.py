de7f317075b51d36962005dbf306b65a848db761
Randall Gann <gannhometech@gmail.com>
2020-10-13 17:08:06 -0500

    initial commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import copy
 
 
-cap = cv2.VideoCapture(0) #"rtsp://admin:admin@192.168.88.97/media/video1"
+cap = cv2.VideoCapture("rtsp://admin:admin@192.168.88.97/media/video1") #"rtsp://admin:admin@192.168.88.97/media/video1"
 
 if not cap.isOpened():
 	print("Error opening video stream or file")
@@ -15,13 +15,16 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 video_out = cv2.VideoWriter('heatmap_video.avi', cv2.VideoWriter_fourcc(*'mjpg'), 10.0, (width, height))
             
 
-background_subtractor = cv2.createBackgroundSubtractorMOG2() 
+background_subtractor = cv2.createBackgroundSubtractorMOG2()
 
 first_iteration_indicator = 1
 
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    #save frame as .jpg file to directory ../frames at rate of 1 frame per second
+    #delete oldest frame if number of files exceed 2000
 
     if first_iteration_indicator == 1:
 
@@ -52,6 +55,12 @@ while(cap.isOpened()):
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+# Function that generates a heatmap of most recent 5 minutes of frames
+
+# Function that generates a heatmap of most recent 10 minutes of frames
+
+# Function that generates a heatmap of most recent 30 minutes of frames
 
 
 # make_video('./frames/', './output.avi')
